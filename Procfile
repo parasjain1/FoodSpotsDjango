fe8ddf34cf0702f8093caa9aba1889a0bb94cd07
monp97 <monika.pandey97@gmail.com>
2018-01-01 17:01:43 +0530

    static files trial1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn djangoApp.wsgi
+web: python manage.py collectstatic --noinput; gunicorn djangoApp.wsgi
--- foodspot/urls.py
+++ foodspot/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += patterns('',
+    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
