c0175ec18d1894500985eff614ed79099c923bb6
monp97 <monika.pandey97@gmail.com>
2018-01-01 12:01:18 +0530

    db settings changed

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -97,10 +98,13 @@ DATABASES = {
         'NAME': 'foodspot',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
